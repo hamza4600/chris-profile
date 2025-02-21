721d443ba9a82c86075c04d03b443c5eac12cf99
hamza4600 <hkhantanoli@gmail.com>
2025-02-21 23:52:07 +0500

    added changes to roboto.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,48 +10,8 @@ export default function robots(): MetadataRoute.Robots {
         disallow: [
           '/studio',       // Block Sanity Studio
           '/api',          // Block API routes
-          '/admin',        // Block admin panels
-          '/_next',        // Block Next.js internals
-          '/private',      // Block private areas
-          '/*?*',          // Block all URLs with query parameters
-          '/*.json$',      // Block JSON files
         ]
       },
-      {
-        userAgent: 'Googlebot',
-        allow: [
-          '/',
-          '/work',         // Allow work section
-          '/blog',         // Allow blog section
-          '/sitemap.xml',
-          '/robots.txt'
-        ],
-        disallow: [
-          '/studio',
-          '/api',
-          '/_next',
-          '/private'
-        ]
-      },
-      {
-        userAgent: [
-          'Bingbot',
-          'DuckDuckBot',
-          'YandexBot',
-          'Slurp',
-          'Baiduspider'
-        ],
-        allow: '/',
-        disallow: [
-          '/studio',
-          '/api',
-          '/private'
-        ]
-      },
-      {
-        userAgent: 'ia_archiver',
-        disallow: '/studio'
-      }
     ],
     sitemap: [
       `${domainURL}/sitemap.xml`,
